efaefcad0334b8822fb4dfbc19151186f47ab962
ckafrouni <ckafrouni@proton.me>
2024-01-17 19:21:50 +0100

    Add parser-combinators

---
+++ parser-combinators/src/app.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { render, Text } from 'ink';
+
+
+const App = () => {
+
+    return (
+        <>
+            <Text color="green">Hello World</Text>
+        </>
+    );
+};
+
+render(<App />);
